a4456b7b45ccd326c24bc205ac0ca7d9d82822f3
Keulen Maia <keulensilva17@gmail.com>
2022-05-29 19:59:39 -0300

    Atividade finalizada

--- cadastro_pecas.js
+++ cadastro_pecas.js
@@ -1,5 +1,15 @@
-let listaPecas = 3;
+let listaPecas = 30;
 let peso = 150;
 let nomePeca = "Cilindro Mestre";
 
-if()
+if(peso >= 100){
+  console.log("Peça tem peso suficiente para cadastrar!");
+}
+if(listaPecas >= 10){
+  console.log("A lista está cheia, não tem capacidade suficiente para cadastrar!");
+}
+if(nomePeca.length < 3){
+  console.log("Nome muito curto!");
+}
+
+console.log("Fim do programa!");
